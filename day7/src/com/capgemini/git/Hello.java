1da4bec52a75229cde16d49b66491f6e3df9be4d
Nikhilm2000 <88482838+Nikhilm2000@users.noreply.github.com>
2022-03-21 16:40:55 +0530

    pull request

---
+++ day7/src/com/capgemini/git/Hello.java
@@ -0,0 +1,11 @@
+package com.capgemini.git;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Git testing for cloning");
+		
+		//pull request
+		System.out.println("pull request ---sub branches");
+	}
+}
